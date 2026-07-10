276b365bfa65bf646aacaac474b7abc95624bb4e
maher yasser <maheryasser550@gmail.com>
2026-07-10 12:16:06 +0300

    Update BaseTest.java

--- Automation_Testing/SawgLabs_GP/src/test/java/Tests/BaseTest.java
+++ Automation_Testing/SawgLabs_GP/src/test/java/Tests/BaseTest.java
@@ -15,11 +15,8 @@ public class BaseTest {
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setUp(@Optional("chrome") String browser,
-                      @Optional("true") String headless)
-    {
+    public void setUp(@Optional("chrome") String browser, @Optional("true") String headless) {
         EdgeOptions options = new EdgeOptions();
-
         options.addArguments("--headless=new");
         options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1920,1080");
@@ -27,20 +24,16 @@ public class BaseTest {
         options.addArguments("--disable-dev-shm-usage");
 
         driver = new EdgeDriver(options);
-
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://www.saucedemo.com/");
     }
 
     @AfterMethod
     public void tearDown(ITestResult result) throws InterruptedException {
-
         if (result.getStatus() == ITestResult.FAILURE) {
             Screenshot.takeScreenshot(driver, result.getName());
         }
-
         Thread.sleep(3000);
         driver.quit();
     }
-}
 }
